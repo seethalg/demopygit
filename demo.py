bf00edbe24f19171b5db7184a0e6851529313c8b
Seetha LG <seetha_lg@yahoo.co.uk>
2020-09-19 17:03:09 +0100

    Testing Commit

--- demo.py
+++ demo.py
@@ -1,1 +1,14 @@
+import re
 print("hello")
+i = 1
+
+while i > 0:
+    print(i,":",end="")
+    i -=2
+    fav = int(input("What is your age:"))
+    if fav > 14:
+        print("you are older than me!!!")
+    else:
+        re.sub(" You are a child", " ", "a")
+        print("You are a child")
+
